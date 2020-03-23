5a23e78b41254201416a3c5bdc92f37fb15ff756
mitarthjain <34244395+mitarthjain@users.noreply.github.com>
2020-03-23 09:13:18 +0530

    Create MiserMan.java

---
+++ MiserMan.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+  
+
+	
+	public static void main(String[] args) {
+		// Write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] a=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                a[i][j]=sc.nextInt();
+                
+            }
+        }
+        int[][] c=new int[n][m];
+        for(int i=0;i<m;i++)
+            c[n-1][i]=a[n-1][i];
+        for(int i=n-2;i>=0;i--){
+            c[i][0]=a[i][0]+Math.min(c[i+1][0],c[i+1][1]);
+            c[i][m-1]=a[i][m-1]+Math.min(c[i+1][m-1],c[i+1][m-2]);
+            for(int j=1;j<m-1;j++){
+                c[i][j]=a[i][j]+Math.min(c[i+1][j-1],Math.min(c[i+1][j],c[i+1][j+1]));
+            }
+      }
+    int k=Integer.MAX_VALUE;
+        for(int i=0;i<m;i++){
+            k=Math.min(k,c[0][i]);
+        }
+        System.out.println(k);
+        
+        
+
+	}
+
+}
